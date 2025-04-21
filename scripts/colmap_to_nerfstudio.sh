c9e40a82348009d5f5d5069a7799b9e1b708df5d
JH-Choi <jaehoonc44@gmail.com>
2025-04-21 00:00:52 -0400

    init colmap_to_nerfstudio

---
+++ scripts/colmap_to_nerfstudio.sh
@@ -0,0 +1,6 @@
+
+
+# conda activate nerfstudiov0.3
+
+# ROOT_DIR=/data/VisDrone/VisDrone-VID/GS_data/uav0000084_00000_v/preprocessed
+# python ./converter/colmap_to_nerfstudio.py --root_dir $ROOT_DIR
